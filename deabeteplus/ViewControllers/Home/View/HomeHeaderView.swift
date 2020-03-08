2aca409e26f4216666a7f38c5d140c4d631b5236
pasin007 <filza009@gmail.com>
2020-03-08 19:15:54 +0700

    8Mar2020

---
+++ deabeteplus/ViewControllers/Home/View/HomeHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeHeaderView.swift
+//  deabeteplus
+//
+//  Created by pasin on 8/3/2563 BE.
+//  Copyright © 2563 Ji Ra. All rights reserved.
+//
+
+import UIKit
+
+class HomeHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
